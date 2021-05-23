bbead1cdf1b409a86646f2710fe9e640098f8206
Kenkentake <keeeentake0908@gmail.com>
2021-05-23 02:52:29 +0000

    Add __init__.py in dataset dir

---
+++ data/dataset/__init__.py
@@ -0,0 +1,10 @@
+from data.datasets.dataloader import set_dataloader
+from data.datasets.dataset_cifar10 import CIFAR10Dataset
+from data.datasets.transforms import set_transforms
+
+__all__  = [
+    "set_dataloader",
+    "CIFAR10Dataset",
+    "set_transforms"
+    ]
+
